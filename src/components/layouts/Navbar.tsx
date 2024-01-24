c08aa7254932bde7de619822e412a3c06c5d0f88
abdurrahmanrahat <abdurrahmanrahat47@gmail.com>
2024-01-24 22:39:05 +0600

    header and container

--- src/components/layouts/Navbar.tsx
+++ src/components/layouts/Navbar.tsx
@@ -1,5 +1,21 @@
+import { NavLink } from "react-router-dom";
+import { Button } from "../ui/button";
+
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <header className="h-16">
+      <nav className="w-full h-full max-w-[1220px] px-[20px] mx-auto flex justify-between items-center bg-green-500">
+        <span className="text-[32px]">iRepair</span>
+        <ul className="space-x-5">
+          <NavLink to="/">Home</NavLink>
+          <NavLink to="/about">About</NavLink>
+          <Button>
+            <NavLink to="/login">Login</NavLink>
+          </Button>
+        </ul>
+      </nav>
+    </header>
+  );
 };
 
 export default Navbar;
